050c169fda2951f9b5e228cc847d5356470b8d96
shubham <shubhamchoudhary20.svc@gmail.com>
2020-10-24 20:44:22 +0530

    added tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='this is another test')
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        def test_view_url_by_name(self):
+            resp = self.client.get(reverse('home'))
+            self.assertEqual(resp.status_code, 200)
+        def test_view_uses_correct_template(self):
+            resp = self.client.get(reverse('home'))
+            self.assertEqual(resp.status_code, 200)
+            self.assertTemplateUsed(resp, 'home.html')
